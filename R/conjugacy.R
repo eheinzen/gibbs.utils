b1936549cd96c1bba3d0db74fd7092262b36eb79
Ethan Heinzen <heinzen.ethan@mayo.edu>
2021-06-07 16:02:40 -0500

    conjugate distributions

---
+++ R/conjugacy.R
@@ -0,0 +1,159 @@
+
+#' Draw Conjugate Samples
+#'
+#' Sample from the posterior (conditional on all other parameters) in the conjugate setting.
+#'
+#' @param y realizations from the distribution whose parameter is being drawn
+#' @param Q,tau the precision of the (multivariate) normal distribution from which \code{y} comes
+#' @param mu0 the prior mean of \code{mu} or \code{beta}.
+#' @param Q0,tau0 the prior precision of \code{mu}.
+#' @param mult An optional multiplier for the prior precision; useful in some cases
+#' @param mu the mean of the normal distribution from which \code{y} comes
+#' @param a0,b0 the parameters (shape and rate) of the gamma distribution prior on \code{tau}.
+#' @param V0,v0 the parameters (matrix and degrees of freedom) of the Wishart prior on \code{Q} or \code{V}
+#' @param V,U the precision matrices for the matrix-normal distribution
+#' @param X the data matrix on \code{beta}
+#' @param beta the coefficients on \code{X}
+#' @param XtX,V0_inv,Xbeta pre-computed "shortcut" arguments for efficiency reasons
+#' @rdname conjugacy
+#' @export
+conj_norm_mu <- function(y, tau, mu0 = 0, tau0 = 0.001, mult = 1) {
+  n <- length(y)
+  newtau <- mult*tau0 + n*tau
+  rnorm(
+    1,
+    mean = (tau0*mu0 + tau*sum(y)) / newtau,
+    sd = 1 / sqrt(newtau)
+  )
+}
+
+#' @rdname conjugacy
+#' @export
+conj_mvnorm_mu <- function(y, Q, mu0 = rep_len(0, n), Q0 = diag(0.001, n), mult = 1) {
+  n <- nrow(y)
+  newQ <- mult*Q0 + n*Q
+  newQ.inv <- my_inv(newQ)
+  MASS::mvrnorm(
+    1,
+    mu = newQ.inv %*% (Q0 %*% mu0 + Q %*% colSums(y)),
+    Sigma = newQ.inv
+  )
+}
+
+#' @rdname conjugacy
+#' @export
+conj_matnorm_mu <- function(y, V, U = NULL, mu0, Q0) {
+  y <- as.numeric(y)
+  if(is.null(U)) U <- diag(nrow(Q0) / nrow(V))
+  VxU <- V %x% U
+  newQ <- VxU + Q0
+  newQ.inv <- my_inv(newQ)
+  MASS::mvrnorm(
+    1,
+    mu = newQ.inv %*% (Q0 %*% mu0 + VxU %*% y),
+    Sigma = newQ.inv
+  )
+}
+
+#' @rdname conjugacy
+#' @export
+conj_lm_beta <- function(y, X, XtX = crossprod(X), tau, mu0, Q0) {
+  newQ <- tau * XtX + Q0
+  newQ.inv <- my_inv(newQ)
+  MASS::mvrnorm(
+    1,
+    mu = newQ.inv %*% (Q0 %*% mu0 + tau*t(X) %*% y),
+    Sigma = newQ.inv
+  )
+}
+
+#' @rdname conjugacy
+#' @export
+conj_matlm_beta <- function(y, X, V, U = NULL, mu0, Q0) {
+  y <- as.numeric(y)
+  mu0 <- as.numeric(mu0)
+
+  XtU <- if(is.null(U)) t(X) else crossprod(X, U)
+  newQ <- V %x% (XtU %*% X) + Q0
+  newQ.inv <- my_inv(newQ)
+  MASS::mvrnorm(
+    1,
+    mu = newQ.inv %*% (Q0 %*% mu0 + (V %x% XtU) %*% y),
+    Sigma = newQ.inv
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' @rdname conjugacy
+#' @export
+conj_norm_tau <- function(y, mu, a0 = 0.001, b0 = 0.001) {
+  stats::rgamma(
+    1,
+    a0 + 0.5*length(y),
+    b0 + 0.5*sum((y - mu)^2)
+  )
+}
+
+#' @rdname conjugacy
+#' @export
+conj_mvnorm_Q <- function(y, mu, V0, v0, V0_inv = my_inv(V0)) {
+  n <- nrow(y)
+  stopifnot(identical(dim(y), dim(mu)))
+  xmu <- y - mu
+  tmp <- Reduce(`+`, lapply(1:n, function(i) {
+    txmu <- xmu[i, , drop = FALSE]
+    t(txmu) %*% txmu
+  }))
+  V2 <- my_inv(V0_inv + tmp)
+  rwish(
+    V = V2,
+    v = n + v0
+  )
+}
+
+#' @rdname conjugacy
+#' @export
+conj_matnorm_V <- function(y, mu, U = NULL, V0, v0, V0_inv = my_inv(V0)) {
+  if(!is.matrix(y) || !is.matrix(mu)) stop("'y' and 'mu' must be matrices")
+  n <- nrow(mu)
+  stopifnot(identical(dim(y), dim(mu)))
+  ymu <- y - mu
+  ymu2 <- if(is.null(U)) t(ymu) else crossprod(ymu, U)
+
+  V2 <- my_inv(V0_inv + ymu2 %*% ymu)
+  rwish(
+    V = V2,
+    v = n + v0
+  )
+}
+
+#' @rdname conjugacy
+#' @export
+conj_lm_tau <- function(y, X, beta, Xbeta = X %*% beta, a0 = 0.001, b0 = 0.001) {
+  conj_norm_tau(y = y, mu = Xbeta, a0 = a0, b0 = b0)
+}
+
+#' @rdname conjugacy
+#' @export
+conj_matlm_sigma <- function(y, X, beta, Xbeta = X %*% beta, U = NULL, V0, v0, V0_inv = my_inv(V0)) {
+  conj_matnorm_V(y = y, mu = Xbeta, U = U, v0 = v0, V0_inv = V0_inv)
+}
+
+
+
+
+
+
+
+
---
+++ R/utils.R
@@ -0,0 +1,27 @@
+
+check_one_or_all <- function(x, len) {
+  nm <- deparse(substitute(x))
+  if(length(x) == 1) rep_len(x, len) else if(length(x) == len) x else stop(nm, " must be of length 1 or ", len)
+}
+
+my_inv <- function(x) {
+  chol2inv(chol(x))
+}
+
+
+logit <- function(p) log(p) - log(1 - p)
+expit <- function(x) 1 / (1 + exp(-x))
+
+propose_01 <- function(p, scale, adjust) {
+  logitp <- logit(p)
+  sd <- sqrt(abs(logitp)+1)*scale*(0.8^adjust)
+  sd <- ifelse(sd < 0.01, 0.01, sd)
+  expit(stats::rnorm(length(logitp), logitp, sd))
+}
+
+propose_01_logdensity <- function(p, proposal, scale, adjust) {
+  logitp <- logit(p)
+  sd <- sqrt(abs(logitp)+1)*scale*(0.8^adjust)
+  sd <- ifelse(sd < 0.01, 0.01, sd)
+  dlogitnorm(proposal, mean = logitp, sd = sd, log = TRUE)
+}
