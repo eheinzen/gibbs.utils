b500b6ed5366d22bd63f6a3bb7673d04b7790e6d
Ethan Heinzen <heinzen.ethan@mayo.edu>
2021-06-09 14:23:24 -0500

    export utils

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# v0.1.1
+
+- Export `logit()` and `expit()`
+
 # v0.1.0
 
 Initial version of package
--- R/utils.R
+++ R/utils.R
@@ -8,11 +8,25 @@ check_one_or_all <- function(x, len) {
   if(length(x) == 1) rep_len(x, len) else if(length(x) == len) x else stop(nm, " must be of length 1 or ", len)
 }
 
+#' Other Utilities
+#'
+#' @param x A matrix for \code{my_inv} or a numeric vector for \code{expit}
+#' @param p A numeric vector
+#' @rdname utilities
+#' @export
 my_inv <- function(x) {
   chol2inv(chol(x))
 }
 
+#' @rdname utilities
+#' @export
+logit <- function(p) {
+  log(p) - log(1 - p)
+}
 
-logit <- function(p) log(p) - log(1 - p)
-expit <- function(x) 1 / (1 + exp(-x))
+#' @rdname utilities
+#' @export
+expit <- function(x) {
+  1 / (1 + exp(-x))
+}
 
